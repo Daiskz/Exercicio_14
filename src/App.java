2a29f5bf35299681030d92ed616610edfea7a233
gabriel.matsubara@gmail.com <gabriel.matsubara@gmail.com>
2023-08-11 16:19:44 -0300

    Mudanças no projeto

--- src/App.java
+++ src/App.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Informe quantos quilos de peixe você possui: ");
-        double kg = entrada.nextDouble();
-        double peso = kg - 50;
-        double multa = peso * 4;
-        System.out.println("Você passou do peso máximo em "+ peso + " quilos e o valor da multa é de: "+ multa + "R$");
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Informe quantos quilos de peixe você possui: ");
+            double kg = entrada.nextDouble();
+            double excesso = kg - 50;
+            double multa = excesso * 4;
+            System.out.println("Você passou do peso máximo em "+ excesso + " quilos e o valor da multa é de: "+ multa + "R$");
+        }
     }
 }
